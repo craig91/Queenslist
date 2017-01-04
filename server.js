117343a302d8f4c8345cf73058f3a0b7d99bc047
kaladin9017 <sanchochris@gmail.com>
2017-01-04 14:19:53 -0500

    added express and placeholder routes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+const path = require('path');
+const bodyParser = require('body-parser');
+
 
 let app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.listen(3000);
