5063337fed8b651c4fcf20401a978cd26c602ee9
kaladin9017 <sanchochris@gmail.com>
2017-01-05 12:19:21 -0500

    a

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
 import express from 'express';
-<<<<<<< HEAD
-const path = require('path');
-const bodyParser = require('body-parser');
 
-
-let app = express();
-
-// app.use(express.static('public'));
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
-
-
-app.get('/api/tags', (req, res) => {
-  // Do Something
-  //
-});
-
-
-
-=======
 import Sequelize from 'sequelize';
-import sequelizeConnection from './sequelizeConnection';
+import sequelizeConnection from './sequelize-connection';
 import bodyParser from 'body-parser';
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
->>>>>>> 87b8cb5f6daecb36ceab971ec7a920f62459eda7
 
 app.listen(3000);
